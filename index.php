809c8da9693645996d358b92222ab3f32309fa6c
Marzida Hashim <babyina99@gmail.com>
2013-04-18 16:51:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-header('location:/parlimen/'); 
+header('location:parlimen/'); 
 //header('location:https://aplikasi.treasury.gov.my/ssjp/parlimen/'); 
 // header( 'Location: http://www.yoursite.com/new_page.html' ) ;
 //header('location:parlimen/');asal
